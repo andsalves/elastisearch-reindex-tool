add369a2d0aa07e3ba01668ef7aa71e6db85c060
Anderson <ands.alves.nunes@gmail.com>
2017-05-31 01:05:40 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
 
 return array(
-    'elasticsearch_host' => 'http://admin:admin@040ad8d14497b2c72da5d4c5e0739320.us-east-1.aws.found.io:9200',
-//    'elasticsearch_host' => 'localhost:9200',
+    'elasticsearch_host' => getenv('ELASTICSEARCH_HOST')
 );
